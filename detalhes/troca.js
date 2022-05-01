7b10aa38ee7e3732125b4fddcd25c4dd5cbd213b
Yasmin Ramos <yasmincoelhoramos@gmail.com>
2022-05-01 02:18:30 -0300

    funções de filtro e pesquisa

---
+++ detalhes/troca.js
@@ -0,0 +1,12 @@
+
+ function troca(){
+    /*const api = axios.create({
+        baseURL: 'https://backprovi54.herokuapp.com',
+    })
+    const listaProdutos = await api.get('/produtos');
+    //console.log(listaProdutos);*/
+    var resultados = document.getElementById("resultados");
+    resultados.style.display = "block";
+
+}
+
